fec0f60bac446ffbd80934fe785225e291e2867e
DemiPixel <luke5227@gmail.com>
2015-11-04 13:21:42 -0800

    Add pitches

--- indexer.js
+++ indexer.js
@@ -1,16 +1,42 @@
+function buildIndexFromObject(object,fieldToIndex) {
+  return Object.keys(object).reduce(function(index,key){
+    index[object[key][fieldToIndex]]=object[key];
+    return index;
+  },{});
+}
+
+function buildIndexFromArray(array, fieldToIndex) {
+  return array.reduce(function(index,element){
+    index[element[fieldToIndex]]=element;
+    return index;
+  },{});
+}
+
+function buildArrayIndexFromArray(array, fieldToIndex) {
+  return array.reduce(function(index,element){
+    var e=index[element[fieldToIndex]]
+    e ? e.push(element) : index[element[fieldToIndex]] = [element];
+    return index;
+  },{});
+}
+
+function buildDoubleIndexFromArray(array, fieldToIndexOne, fieldToIndexTwo) {
+  if (typeof fieldToIndexTwo != 'object') fieldToIndexTwo = [fieldToIndexTwo];
+  var inter = buildArrayIndexFromArray(array, fieldToIndexOne);
+  return Object.keys(inter).reduce(function(index,key){
+    index[key] = inter[key].reduce(function(ind, k) {
+      for (var i = 0; i < fieldToIndexTwo.length; i++) {
+        if (k[fieldToIndexTwo[i]]) ind[k[fieldToIndexTwo[i]]] = k;
+      }
+      return ind;
+    },{});
+    return index;
+  },{});
+}
+
+
 module.exports={
-  buildIndexFromObject:
-    function(object,fieldToIndex) {
-      return Object.keys(object).reduce(function(index,key){
-        index[object[key][fieldToIndex]]=object[key];
-        return index;
-      },{});
-    },
-  buildIndexFromArray:
-    function(array,fieldToIndex) {
-      return array.reduce(function(index,element){
-        index[element[fieldToIndex]]=element;
-        return index;
-      },{});
-    }
+  buildIndexFromObject: buildIndexFromObject,
+  buildIndexFromArray: buildIndexFromArray,
+  buildDoubleIndexFromArray: buildDoubleIndexFromArray
 };
--- indexes.js
+++ indexes.js
@@ -16,6 +16,9 @@ module.exports= function(mcData){
     itemsByName:indexer.buildIndexFromArray(mcData.items,"name"),
 
     windowsById:indexer.buildIndexFromArray(mcData.windows,"id"),
-    windowsByName:indexer.buildIndexFromArray(mcData.windows,"name")
+    windowsByName:indexer.buildIndexFromArray(mcData.windows,"name"),
+
+    pitchesByNote:indexer.buildDoubleIndexFromArray(mcData.pitches, "octave", ["key1", "key2"]),
+    pitchesByNum:indexer.buildIndexFromArray(mcData.pitches, "num")
   };
 };

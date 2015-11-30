154ceac7fdb4a06cd5b9350c279e86084caab683
Romain Beaumont <romain.rom1@gmail.com>
2015-11-30 12:58:24 +0100

    use require-self to use require('minecraft-data') in example and make tonicdev actually work

--- example.js
+++ example.js
@@ -1,9 +1,6 @@
-var mcData=require("./")("1.8.8");
+var mcData=require("minecraft-data")("1.8.8");
 
 console.log(mcData.blocksByName["stone"]);
-
 console.log(mcData.windows["minecraft:brewing_stand"]);
-
 console.log(mcData.version);
-
 console.log(mcData.effectsByName["Haste"]);

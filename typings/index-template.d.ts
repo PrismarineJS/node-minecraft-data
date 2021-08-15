8f4998376f13d0594f95b744e79a029a7a76b093
extremeheat <extreme@protonmail.ch>
2021-08-15 03:50:07 -0400

    add version methods to docs

--- typings/index-template.d.ts
+++ typings/index-template.d.ts
@@ -1,3 +1,17 @@
+// This will interface will merge with the generated one
+export interface Version {
+    // Returns true of the current version is greater than or equal to the `other` version's dataVersion
+    ['>='](other)
+    // Returns true of the current version is greater than the `other` version's dataVersion
+    ['>'](other)
+    // Returns true of the current version is less than the `other` version's dataVersion
+    ['<'](other)
+    // Returns true of the current version is less than than or equal to the `other` version's dataVersion
+    ['<='](other)
+    // Returns true of the current version is equal to the `other` version's dataVersion
+    ['=='](other)
+    type: 'pc' | 'bedrock'
+}
 
 export interface VersionSet {
     pc: { [version: string]: Version };

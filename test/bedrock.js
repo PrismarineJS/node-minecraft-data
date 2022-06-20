d13b9573c08ba500da4c06ae0aadb5deb0705d9b
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-06-20 06:34:18 -0400

    update `md-release`

--- test/bedrock.js
+++ test/bedrock.js
@@ -1,27 +1,27 @@
 /* eslint-env mocha */
 
-// const assert = require('assert');
-// const packets = require('minecraft-packets').bedrock;
+// const assert = require('assert')
+// const packets = require('minecraft-packets').bedrock
 
 // function test(version) {
-//     console.log('->', version);
-//     const packet = packets[version]['from-server'];
-//     const mcd = require('minecraft-data')('bedrock_' + version);
-//     if (!mcd.items) {
-//         console.log(`Don't have data for '${version}', skipping`);
-//         return;
-//     }
-//     mcd.loadItemPalette(packet.start_game[-1].json.itemstates);
-//     const { item, block } = mcd.findItemByName('acacia_trapdoor');
-//     assert.ok(item);
-//     assert.ok(block);
-//     assert.strictEqual(mcd.findItemByRuntimeId(1).blockId, 1);
+//   console.log('->', version)
+//   const packet = packets[version]['from-server']
+//   const mcd = require('minecraft-data')('bedrock_' + version)
+//   if (!mcd.items) {
+//     console.log(`Don't have data for '${version}', skipping`)
+//     return
+//   }
+//   mcd.loadItemPalette(packet.start_game[-1].json.itemstates)
+//   const { item, block } = mcd.findItemByName('acacia_trapdoor')
+//   assert.ok(item)
+//   assert.ok(block)
+//   assert.strictEqual(mcd.findItemByRuntimeId(1).blockId, 1)
 // }
 
 // describe('can query bedrock items', () => {
-//     for (const version in packets) {
-//         it('works on ' + version, () => {
-//             test(version);
-//         });
-//     }
-// });
+//   for (const version in packets) {
+//     it('works on ' + version, () => {
+//       test(version)
+//     })
+//   }
+// })

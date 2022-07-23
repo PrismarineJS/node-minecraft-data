54bb7b86b5011c3a612cd74b4f87003744f2a626
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-07-23 09:45:10 -0400

    Revert "Remove `foodsByFoodPoints` and `foodsBySaturation`"
    This reverts commit 123c6a64a8515def632330c7589564ec4de72b55.

--- typings/index-template.d.ts
+++ typings/index-template.d.ts
@@ -109,6 +109,8 @@ export interface IndexedData {
     foods: { [id: number]: Food; };
     foodsByName: { [name: string]: Food; };
     foodsArray: Food[];
+    foodsByFoodPoints: { [foodPoints: number]: Food[]; };
+    foodsBySaturation: { [saturation: number]: Food[]; };
 
     biomes: { [id: number]: Biome; };
     biomesArray: Biome[];

2d29eba0f6508bdb5d98959a13263b3c3c4d1af8
Vitaly <vital2580@icloud.com>
2024-08-31 19:01:52 +0300

    regression: do not make properties of block optional

--- typings/test-typings.ts
+++ typings/test-typings.ts
@@ -34,6 +34,7 @@ console.log(getMcData('bedrock_0.14').version)
 
 console.log(getMcData('pc_1.9').blocksByName['dirt'])
 console.log(getMcData('pc_1.9').blocksByName['dirt'].minStateId.toExponential)
+console.log(getMcData('pc_1.9').blocksByName['dirt'].name.includes)
 
 console.log(getMcData('pc_1.9').protocol.toClient)
 
